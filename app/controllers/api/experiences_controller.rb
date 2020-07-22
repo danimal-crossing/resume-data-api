cfa247f975fc11e34dab10e0b5254bd3e17a1ce9
luke <lddeitz@gmail.com>
2020-07-22 13:18:35 -0500

    adds show, update, and destroy to the controller

--- app/controllers/api/experiences_controller.rb
+++ app/controllers/api/experiences_controller.rb
@@ -17,15 +17,28 @@ class Api::ExperiencesController < ApplicationController
     render json: {message: "Experience successfully added!"}
   end 
 
-  # def show
-  #   @experience = Experience.find(params[:id])
-  #     start_date: @experience.start_date,
-  #     end_date: @experience.end_date,
-  #     job_title: @experience.job_title,
-  #     company_name: @experience.company_name,  
-  #     details: @experience.details,
-  #     student_id: @experience.student_id
-  #   render "show.json.jb" #come back to this
-  # end 
+  def show
+    @experience = Experience.find(params[:id])
+    render "show.json.jb"
+  end 
+
+  def update
+    @experience = Experience.find(params[:id])
+
+    @experience.start_date == params[:start_date] ||@experience.start_date,
+    @experience.end_date == params[:end_date] ||@experience.end_date,
+    @experience.job_title == params[:job_title] ||@experience.job_title,
+    @experience.company_name == params[:company_name] ||@experience.company_name,
+    @experience.details == params[:details] ||@experience.details,
+    @experience.student_id == params[:student_id] ||@experience.student_id,
+    
+    @experience.save
+    render "show.json.jb"
+  end 
+
+  def destroy
+    @experience = Experience.find(params[:id])
+    @experience.destroy
+  end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
     get "/experiences" => "experiences#index"
     post "/experiences" =>
     "experiences#create"
-    # get "/experiences/:id" => "experiences#show"
-    # patch "/experiences/:id" => "experiences#update"
-    # delete "/experiences/:id" => "experiences#destroy"
+    get "/experiences/:id" => "experiences#show"
+    patch "/experiences/:id" => "experiences#update"
+    delete "/experiences/:id" => "experiences#destroy"
   end
 end
